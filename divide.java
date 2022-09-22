0571e171f702ffd1be298adddd3bdc13b275e9b9
Rahul Goswami <rahulgoswami8112@gmail.comgit config --global user.email git config --global user.email rahulgoswami8112@gmail.com>
2022-09-22 15:19:10 +0530

    add divide file

---
+++ divide.java
@@ -0,0 +1,9 @@
+public class divide {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        int a=23;
+        int b=45;
+        int c=(a+b);
+        System.out.println("The sum is "+ c);
+    
+}
